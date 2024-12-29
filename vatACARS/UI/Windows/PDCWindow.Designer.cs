823b69fb07654db9466f9a0ef20d29a1d357ddff
AussieScorcher <67866285+AussieScorcher@users.noreply.github.com>
2024-12-30 03:17:56 +0800

    Replace vScrollBar1 with scl_aircraft and update ListView
    - Replaced `vScrollBar1` with `scl_aircraft` in `PDCWindow.Designer.cs`
    - Adjusted `strips_aircraft` ListView size from `464, 357` to `464, 374`
    - Added `MouseWheel` event handler to `strips_aircraft`
    - Removed `label1` control
    - Added `scl_aircraft` control with `Scroll`, `Scrolling`, and `MouseWheel` event handlers
    - Adjusted `ClientSize`, `MaximumSize`, and `MinimumSize` of `PDCWindow` form
    - Added `hoverPoint` field to `PDCWindow.cs`
    - Expanded `aircraftList` with duplicate entries for testing
    - Updated `strips_aircraft` ListView to use custom drawing for items and sub-items
    - Added `MouseMove` and `MouseLeave` event handlers to `strips_aircraft` for hover effects
    - Updated `LoadStrips` method to remove sub-item styling and call `UpdateScrollbar`
    - Added `scl_aircraft_Scroll` method to sync scrollbar with ListView
    - Updated `PDCWindow_Resize` method to call `UpdateScrollbar`
    - Added `UpdateScrollbar` method to adjust scrollbar based on ListView content height
    - Added `strips_aircraft_MouseWheel` method for mouse wheel scrolling
    - Updated `StyleWindow` method to style `scl_aircraft` control
    - Moved `Aircraft` class to a comment suggesting it be moved to a separate file

--- vatACARS/UI/Windows/PDCWindow.Designer.cs
+++ vatACARS/UI/Windows/PDCWindow.Designer.cs
@@ -22,8 +22,7 @@ namespace vatACARS.UI
         private void InitializeComponent()
         {
             this.strips_aircraft = new vatsys.ListViewEx();
-            this.vScrollBar1 = new VATSYSControls.ScrollBar();
-            this.label1 = new System.Windows.Forms.Label();
+            this.scl_aircraft = new VATSYSControls.ScrollBar();
             this.SuspendLayout();
             // 
             // strips_aircraft
@@ -37,58 +36,50 @@ namespace vatACARS.UI
             this.strips_aircraft.Location = new System.Drawing.Point(1, 2);
             this.strips_aircraft.MultiSelect = false;
             this.strips_aircraft.Name = "strips_aircraft";
-            this.strips_aircraft.Size = new System.Drawing.Size(464, 357);
+            this.strips_aircraft.Size = new System.Drawing.Size(464, 374);
             this.strips_aircraft.TabIndex = 1;
             this.strips_aircraft.UseCompatibleStateImageBehavior = false;
+            this.strips_aircraft.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.strips_aircraft_MouseWheel);
             // 
-            // vScrollBar1
+            // scl_aircraft
             // 
-            this.vScrollBar1.ActualHeight = 10;
-            this.vScrollBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.scl_aircraft.ActualHeight = 10;
+            this.scl_aircraft.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.vScrollBar1.Change = 1;
-            this.vScrollBar1.Location = new System.Drawing.Point(471, 2);
-            this.vScrollBar1.Name = "vScrollBar1";
-            this.vScrollBar1.Orientation = System.Windows.Forms.ScrollOrientation.VerticalScroll;
-            this.vScrollBar1.PercentageValue = 0F;
-            this.vScrollBar1.PreferredHeight = 10;
-            this.vScrollBar1.Size = new System.Drawing.Size(20, 357);
-            this.vScrollBar1.TabIndex = 2;
-            this.vScrollBar1.Value = 0;
-            // 
-            // label1
-            // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 362);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 17);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
+            this.scl_aircraft.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.scl_aircraft.Change = 1;
+            this.scl_aircraft.Location = new System.Drawing.Point(467, 2);
+            this.scl_aircraft.Name = "scl_aircraft";
+            this.scl_aircraft.Orientation = System.Windows.Forms.ScrollOrientation.VerticalScroll;
+            this.scl_aircraft.PercentageValue = 0F;
+            this.scl_aircraft.PreferredHeight = 10;
+            this.scl_aircraft.Size = new System.Drawing.Size(20, 374);
+            this.scl_aircraft.TabIndex = 2;
+            this.scl_aircraft.Value = 0;
+            this.scl_aircraft.Scroll += new System.EventHandler(this.scl_aircraft_Scroll);
+            this.scl_aircraft.Scrolling += new System.EventHandler(this.scl_aircraft_Scroll);
+            this.scl_aircraft.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.strips_aircraft_MouseWheel);
             // 
             // PDCWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(493, 388);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.vScrollBar1);
+            this.ClientSize = new System.Drawing.Size(489, 388);
+            this.Controls.Add(this.scl_aircraft);
             this.Controls.Add(this.strips_aircraft);
-            this.MaximumSize = new System.Drawing.Size(497, 900);
+            this.MaximumSize = new System.Drawing.Size(493, 900);
             this.MiddleClickClose = false;
-            this.MinimumSize = new System.Drawing.Size(497, 137);
+            this.MinimumSize = new System.Drawing.Size(493, 137);
             this.Name = "PDCWindow";
             this.Text = "Pre-Departure Clearance";
             this.Resize += new System.EventHandler(this.PDCWindow_Resize);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private vatsys.ListViewEx strips_aircraft;
-        private VATSYSControls.ScrollBar vScrollBar1;
-        private Label label1;
+        private VATSYSControls.ScrollBar scl_aircraft;
     }
 }
