c74ea223c1a4657a201e2bd063cd8ade62408895
Joshua Hughes <joshua@plutonus.dev>
2024-12-29 03:24:04 +0100

    Refactor Logger class to public and update logging functionality with file output

--- vatACARS/Services/Logging/Logger.cs
+++ vatACARS/Services/Logging/Logger.cs
@@ -1,12 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
-namespace vatACARS.Services.Logging
+namespace vatACARS.Util
 {
-    internal class Logger
+    public class Logger
     {
+        private string dirPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\vatACARS";
+        private string name;
+
+        public Logger(string Name)
+        {
+            name = Name;
+        }
+
+        public void Log(string msg)
+        {
+            try
+            {
+                using (StreamWriter w = File.AppendText($"{dirPath}\\vatACARS.log"))
+                {
+                    w.WriteLine("{0} [{1}]: {2}", DateTime.UtcNow.ToLongTimeString(), name, msg);
+                }
+            }
+            catch
+            {
+            }
+        }
     }
 }
