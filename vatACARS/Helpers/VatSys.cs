92315d8b81bb8ba59e6274537526659f504a2ac9
Joshua Hughes <joshua@plutonus.dev>
2024-12-29 03:24:24 +0100

    Add VatSys helper class for retrieving station information based on network status

---
+++ vatACARS/Helpers/VatSys.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+using vatsys;
+
+namespace vatACARS.Helpers
+{
+    public static class VatSys
+    {
+        public static string GetStation()
+        {
+            if (Network.Me.IsRealATC && Network.IsConnected)
+            {
+                if(new[] { "_APP", "_TWR", "_GND", "_DEL" }.Any(suffix => Network.Me.Callsign.EndsWith(suffix)))
+                {
+                    return MMI.PrimePosition.ArrivalListAirports.ToList().FirstOrDefault();
+                }
+
+                return "Y" + Network.Me.Callsign.Split('-')[1].Split('_')[0];
+            } else
+            {
+                return "";
+            }
+        }
+    }
+}
